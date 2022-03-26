2c31e580e8b617b01ee0e378294f2385d0285c2a
Kaleen-MicheleXO <Kaleen.Moon1@gmail.com>
2022-03-25 20:53:11 -0400

    Convert a Boolean to a String Solution 03-25-2022

---
+++ CodeChallenge03-25-22/main.js
@@ -0,0 +1,8 @@
+// Implement a function which convert the given boolean value into its string representation.
+
+// Note: Only valid inputs will be given.
+
+//I will need to write a function that returns a string of "true" or "false"
+//my parameter will have to be strictly equal to a boolean of true
+
+let booleanToString = (b) => (b === true ? "true" : "false");
