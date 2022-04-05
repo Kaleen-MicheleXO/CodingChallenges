ef430126c65852bc5238089f2a1f2d37c2a7531b
Kaleen-MicheleXO <Kaleen.Moon1@gmail.com>
2022-04-05 09:40:03 -0400

    coding challenge and solution 04-05-22

--- CodeChallenge04-05-22/main.js
+++ CodeChallenge04-05-22/main.js
@@ -2,4 +2,4 @@
 
 let makeUpperCase = (str) => str.toUpperCase();
 makeUpperCase("Hello");
-//toUpperCase method returns the value of a string in uppercase letters
+//toUpperCase method returns the value of a string in uppercase letters.
