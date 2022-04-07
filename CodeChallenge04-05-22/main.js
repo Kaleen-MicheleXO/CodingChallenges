468acab48b6be9581280e4c9e41014ce61ba0dbb
Kaleen-MicheleXO <Kaleen.Moon1@gmail.com>
2022-04-07 09:35:14 -0400

    Coding Challenge and solution  04-05-22

---
+++ CodeChallenge04-05-22/main.js
@@ -0,0 +1,5 @@
+//Write a function which converts the input string to uppercase.
+
+let makeUpperCase = (str) => str.toUpperCase();
+makeUpperCase("Hello");
+//toUpperCase method returns the value of a string in uppercase letters
