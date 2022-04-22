e76bcf85cebd43a595aa22ff91a844529d929d94
Kaleen-MicheleXO <Kaleen.Moon1@gmail.com>
2022-04-22 13:16:10 -0400

    Bin to Decimal Solution 04-22-22

---
+++ CodeChallenge04-22-22/main.js
@@ -0,0 +1,8 @@
+//Complete the function which converts a binary number (given as a string) to a decimal number.
+
+//A radix parameter specifies the number system to use.
+//2 = binary, 8 = octal, 10 = decimal, 16 = hexadecimal.
+
+function binToDec(bin) {
+  return parseInt(bin, 2);
+}
